4d90af5f950841f4750dd1b9efc4f7f2d482874a
Mouad Oulouali <mouadoune430@gmail.com>
2025-04-07 18:48:51 -0400

    added card.cs from elevens game

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -1,16 +1,63 @@
-using UnityEngine;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-public class Card : MonoBehaviour
+public class Card
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    //Fields, example: Rank rank;
+    private Rank rank;
+    private Suit suit;
+    private bool faceUp;
+    private int value;
+
+    //Card Constructor
+    public Card(Suit suit, Rank rank)
     {
-        
+        this.suit = suit;
+        this.rank = rank;
+        this.faceUp = false; // making cards start face down by default
+        this.value = CalculateValue(rank);
     }
 
-    // Update is called once per frame
-    void Update()
+    //Define properties for all above fields
+    //code example: public Suit Suit { get { return suit; } }
+    public Suit Suit
     {
-        
+        get
+        {
+            return suit;
+        }
     }
+    public Rank Rank
+    {
+        get
+        {
+            return rank;
+        }
+    }
+    public bool FaceUp
+    {
+        get
+        {
+            return faceUp;
+        }
+    }
+
+    public void FlipOver()
+    {
+        // if face down then face up
+        if (faceUp == false)
+        {
+            faceUp = true;
+        }
+        // if face up then down
+        else
+        {
+            faceUp = false;
+        }
+    }
+
 }

39e388e586d07a62c824f023363c5cac987bd169
Mouad Oulouali <mouadoune430@gmail.com>
2025-04-15 00:07:45 -0400

    added specialized fields, lists for deck and selected card

--- Assets/Scripts/ElevensGame.cs
+++ Assets/Scripts/ElevensGame.cs
@@ -3,20 +3,20 @@ using System;
 using System.Collections.Generic;
 using System.Runtime.InteropServices;
 
-public class ElevensGame 
+public class ElevensGame
 {
     private Deck deck;
     private Board board;
     private int loss_count;
     private int win_count;
-
     // constructor to initialize wins and losses
     public ElevensGame()
     {
         win_count = 0;
         loss_count = 0;
     }
-    public void StarNewGame() {
+    public void StarNewGame()
+    {
         // creating new deck and shuffling it
         deck = new Deck;
         deck.Shuffle();
@@ -25,10 +25,12 @@ public class ElevensGame
         board = new Board();
 
         // dealing first board of cards
-        for (int i =0; i < 9; i++) {
+        for (int i = 0; i < 9; i++)
+        {
             Card card = deck.DealCard();
 
-            if (card != null) {
+            if (card != null)
+            {
                 // turn card face up
                 card.FlipOver();
 
@@ -37,11 +39,13 @@ public class ElevensGame
             }
         }
     }
-    public void selectedCard() {
+    public void selectedCard()
+    {
         Card selectedCard = board.Select(position);
 
         // check if selected cards are valid combination
-        if (board.GetSelectedCards().Count >= 2) {
+        if (board.GetSelectedCards().Count >= 2)
+        {
             SelectinoProcess();
         }
     }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,36 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using TMPro;
 
-public class GameManager
+public class GameManager : MonoBehaviour
 {
-    
+    public static GameManager Instance { get; private set; }
+
+    [Header("Game References")]
+    [SerializeField] private GameObject cardPrefab;
+    [SerializeField] private Transform cardsContainer;
+    [SerializeField] private TextMeshProGUI scoreText;
+    [SerializeField] private TextMeshProGUI deckCountText;
+    [SerializeField] private GameObject gameOverPanel;
+    [SerializeField] private Button submitButton;
+    [SerializeField] private Button newGameButton;
+
+    private ElevensGame game;
+
+    private List<CardController> cardControllers = new List<CardController>();
+    private List<int> selectedPositions = new List<int>();
+
+    private void Awake() { }
+
+    private void Start() { }
+
+    private void Update() { }
+
+
+
+
+
+
 }

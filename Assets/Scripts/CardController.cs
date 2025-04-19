0f670a3acfabcac2c04eb37f01a65cd37594d594
Mouad Oulouali <mouadoune430@gmail.com>
2025-04-18 21:20:25 -0400

    updating cardcontroller

--- Assets/Scripts/CardController.cs
+++ Assets/Scripts/CardController.cs
@@ -1,6 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
-public class CardController
+public class CardController : MonoBehaviour
 {
-    
+    [SerializeField] private Image cardImage;
+    [SerializeField] private Sprite cardBackSprite;
+    [SerializeField] private Sprite[] cardSprites; // using array for all card sprites from the sheet
+
+    private Card cardData;
+    private int boardPosition;
+    private bool isSelected = false;
+
+    // initializing the card with its data and position
+    public void InitializeCard(Card card, int position)
+    {
+        cardData = card;
+        boardPosition = position;
+        UpdateCardVisuals();
+    }
+
+    // getting board position
+    public int GetBoardPosition()
+    {
+        get {
+            return boardPosition;
+        }
+    }
+
+    // getting card data
+    public Card GetCardData()
+    {
+        get {
+            return cardData;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 }
